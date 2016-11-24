0b7abed6c6c220e13fbfa160642ca29d4887c4a4
Kasia Iwańska <sobylmine@gmail.com>
2016-11-24 11:53:19 +0100

    Initial files

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('scss/*.scss')
+		.pipe(sass({errLogToConsole:true, 
+			outputStyle: 'compact'
+        }))
+		.pipe(gulp.dest('css'))
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch('scss/**/*.scss', ['sass']);
+});
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../css/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Hello.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
